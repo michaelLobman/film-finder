a2a8c289ba0773648c3f943014f60285366d2bd4
Michael Lobman <michael.lobman@gmail.com>
2021-11-25 11:29:32 -0500

    Added functionality specific to each input as well as ensured that no film will be duplicated

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function renderPage(){
 	fetchPosters();
 	formListener();
 	recListener();
+	clearListener();
 
 }
+// const watchedArray = [];
+const arrayAgainst = [];
 
-const watchedArray = [];
 
 
 function fetchPosters(){
@@ -29,6 +31,21 @@ function renderPosters(film, container) {
 	newCard.src = `${"https://www.themoviedb.org/t/p/original" + film["poster_path"]}`
 	newCard.className = "film-poster";
 	container.append(newCard); 
+
+	console.log(newCard.parentElement);
+
+	const counter = document.getElementById('input-counter');
+	const watchedContainer = document.getElementById("watched-posters");
+
+	if (container === watchedContainer) {
+		newCard.addEventListener('click', removeCard);
+	}
+}
+
+function removeCard(e) {
+	e.target.remove();
+	const counter = document.getElementById('input-counter');
+	counter.textContent--
 }
 
 function formListener(){
@@ -48,10 +65,17 @@ function fetchInput(e){
 			const inputFilm = data["results"][0];
 			const inputContainer = document.getElementById('watched-posters');
 			renderPosters(inputFilm, inputContainer);
+			// watchedArray.push(inputFilm);
+			// watchedArray.push(inputFilm["id"]);
 
-			watchedArray.push(inputFilm);
+			//this is quite repetivie code. there's a way
+			// to copy the array... but maybe we can use the same
+			// array... right now just trying to have some fix
+			arrayAgainst.push(inputFilm["id"]);
 
 			counter.textContent++
+
+
 
 		} else { 
 			counter.textContent = "You have already entered five films"
@@ -67,6 +91,162 @@ function recListener(){
 	recButton.addEventListener('click', getRecommendations);
 }
 
+function renderUnique (results, container) {
+
+	let i = 0;
+	let x = 0;
+
+	while (x < 2) {
+
+		if (arrayAgainst.includes(results[i]["id"])) {
+			console.log("already logged");
+
+			i++
+
+		} else {
+			renderPosters(results[i], container)
+			arrayAgainst.push(results[i]["id"])
+				
+			x++
+			i++
+		}
+	}
+
+	console.log(arrayAgainst);
+
+}
+
+
+	// this should have inputs of the entered films and 
+	// the films that are being recommended.
+
+	// let's assume they are going to enter five films
+
+	// and the button shouldn't be avaialbe until they do,
+
+	// so then, for each, film, we need two results.
+
+	// even if they five films are the same, it will keep going
+
+	// down the chain until it gets to the ninth and tenth result,
+
+	// and returns that. so the number that really needs to be counter
+
+	// are the two recs per entered film, and the 10 total films entered into 
+
+	// the array, which begins with the five input films.
+
+
+
+// 	const recContainer = document.getElementById("rec-posters");
+
+// 	let x = 0;
+
+// 	while (x < 2) {
+
+// 		for
+
+
+// 	}
+
+
+// 	for (x = 0; x < 2; x++) {
+
+// 		for (i = 0; i < 10; i++) {
+
+// 			if (arrayAgainst.includes(results[i]["id"])) {
+// 				console.log("already logged");
+
+// 			} else {
+// 				results[i]
+// 				renderPosters(results[i], recContainer)
+// 				recArray.push(results[i]["id"])
+// 				console.log(i);
+// 			}
+
+// 		}
+// 	}
+
+// }
+
+
+function getRecommendations () {
+
+	const recContainer = document.getElementById("rec-posters");
+	document.getElementById("default-posters").innerHTML = ''
+
+	// was watchedArray
+
+	for(const watchedId of arrayAgainst) {
+		fetch(`https://api.themoviedb.org/3/movie/${watchedId}/recommendations?api_key=b0b77ea6cc2033f31116d4ef4f5925a6&language=en-US&page=1`)
+		.then(resp => resp.json())
+		.then(data => {
+
+			const films = data["results"]
+		//	const films = data["results"].slice(0, 2);
+			const containerCounter = recContainer.childElementCount;
+
+			renderUnique(films, recContainer);
+
+			// films.forEach(film => {
+
+			// 	renderPosters(film, recContainer);
+			// 	recArray.push(film["id"]);
+
+			// })
+
+		})
+	}
+	
+}
+
+			// need to write a recursive function
+			// it will check each element if it is already included,
+			// when added it will add to a counter i or j, adding up to two elements
+
+
+				/*
+
+				console.log("container count =" + recContainer.childElementCount)
+
+				// this doesnt check second one in
+				// and it doesn't account for the fact that the second element will
+				// 
+
+				for (i = 0; i < 2; i++) {
+
+					if (recArray.includes(results[i]["id"])) {
+
+						renderPosters(results[i+1], recContainer)
+
+						recArray.push(results[i]["id"])
+
+						i++
+
+					} else {
+
+						renderPosters(results[i], recContainer)
+
+						recArray.push(results[i]["id"])
+
+						i++
+
+						
+					}
+
+
+
+				}
+
+
+				})
+				*/
+
+
+
+
+/*
+
 function getRecommendations(){
 	const genreArray = [];
 	for(const film of watchedArray){
@@ -77,9 +257,9 @@ function getRecommendations(){
 	console.log(genreArray.flat());
 
 	renderRecs(genreArray.flat());
-
-
 }
+
+
 
 function renderRecs(array){
 	const genreObj = {};
@@ -101,6 +281,9 @@ function renderRecs(array){
 
 	const finalArr = [];
 
+	const recContainer = document.getElementById("rec-posters");
+
+
 	sortedArr.forEach(genre => finalArr.push(genre[0]))
 
 	const sliced = finalArr.slice(0, 6);
@@ -114,17 +297,86 @@ function renderRecs(array){
 	// clear out others/defaults
 
 	document.getElementById("default-posters").innerHTML = ''
-	const recContainer = document.getElementById("rec-posters");
+
+
+
+	switch (thirdGenre){
+		case 53:
+			fetch("https://api.themoviedb.org/4/list/7419796?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
+			.then(resp => resp.json())
+			.then(data => {
+
+				const filmArray = data["results"].slice(0, 2);
+
+				for (const film of filmArray) {
+					renderPosters(film, recContainer)
+				}
+			})
+			break;
+		case 80:
+			fetch("https://api.themoviedb.org/4/list/7423063?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
+			.then(resp => resp.json())
+			.then(data => {
+
+				const filmArray = data["results"].slice(0, 2);
+
+				for (const film of filmArray) {
+					renderPosters(film, recContainer)
+				}
+			})
+			break;
+		case 18:
+			fetch("https://api.themoviedb.org/4/list/7432068?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
+			.then(resp => resp.json())
+			.then(data => {
+
+				const filmArray = data["results"].slice(0, 2);
+
+				for (const film of filmArray) {
+					renderPosters(film, recContainer)
+				}
+			})
+			break;
+		case 28:
+			fetch("https://api.themoviedb.org/4/list/7434358?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
+			.then(resp => resp.json())
+			.then(data => {
+
+				const filmArray = data["results"].slice(0, 2);
+
+				for (const film of filmArray) {
+					renderPosters(film, recContainer)
+				}
+			})
+			break
+		case 35:
+			fetch("https://api.themoviedb.org/4/list/7441510?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
+			.then(resp => resp.json())
+			.then(data => {
+
+				const filmArray = data["results"].slice(0, 2);
+
+				for (const film of filmArray) {
+					renderPosters(film, recContainer)
+				}
+			})
+			break
+
+	}
+
+	// could probably do these below as switch statements... Think I can abstract out
+	// more of the code?? 
+
+	// maybe the for of can become
+
+	// filmArray.forEach(film => renderPosters(film, recContainer));
+
+
 
 	if (firstGenre === 53 || secondGenre === 53) {
-
-		console.log('inside first')
-
 		fetch("https://api.themoviedb.org/4/list/7419796?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
 		.then(resp => resp.json())
 		.then(data => {
-
-			// below could might not work (the slice)
 
 			const filmArray = data["results"].slice(0, 4);
 
@@ -132,14 +384,10 @@ function renderRecs(array){
 				renderPosters(film, recContainer)
 			}
 		})
-
 	}
 
 
 	if (firstGenre === 80 || secondGenre === 80) {
-
-		console.log('inside second')
-
 		fetch("https://api.themoviedb.org/4/list/7423063?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
 		.then(resp => resp.json())
 		.then(data => {
@@ -153,9 +401,6 @@ function renderRecs(array){
 	}
 
 	if (firstGenre === 18 || secondGenre === 18) {
-
-		console.log('inside second')
-
 		fetch("https://api.themoviedb.org/4/list/7432068?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
 		.then(resp => resp.json())
 		.then(data => {
@@ -166,14 +411,24 @@ function renderRecs(array){
 				renderPosters(film, recContainer)
 			}
 		})
-
 	}
 
 	if (firstGenre === 28 || secondGenre === 28) {
-
-		console.log('inside second')
-
 		fetch("https://api.themoviedb.org/4/list/7434358?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
+		.then(resp => resp.json())
+		.then(data => {
+
+			const filmArray = data["results"].slice(0, 4);
+
+			for (const film of filmArray) {
+				renderPosters(film, recContainer)
+			}
+		})
+	}
+
+
+	if (firstGenre === 35 || secondGenre === 35) {
+		fetch("https://api.themoviedb.org/4/list/7441510?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
 		.then(resp => resp.json())
 		.then(data => {
 
@@ -185,79 +440,9 @@ function renderRecs(array){
 		})
 
 	}
-
-
-	if (thirdGenre === 53) {
-
-		console.log('inside first')
-
-		fetch("https://api.themoviedb.org/4/list/7419796?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
-		.then(resp => resp.json())
-		.then(data => {
-
-			// below could might not work (the slice)
-
-			const filmArray = data["results"].slice(0, 2);
-
-			for (const film of filmArray) {
-				renderPosters(film, recContainer)
-			}
-		})
-
-	}
-
-
-	if (thirdGenre === 80) {
-
-		console.log('inside second')
-
-		fetch("https://api.themoviedb.org/4/list/7423063?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
-		.then(resp => resp.json())
-		.then(data => {
-
-			const filmArray = data["results"].slice(0, 2);
-
-			for (const film of filmArray) {
-				renderPosters(film, recContainer)
-			}
-		})
-	}
-
-	if (thirdGenre === 18) {
-
-		console.log('inside second')
-
-		fetch("https://api.themoviedb.org/4/list/7432068?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
-		.then(resp => resp.json())
-		.then(data => {
-
-			const filmArray = data["results"].slice(0, 2);
-
-			for (const film of filmArray) {
-				renderPosters(film, recContainer)
-			}
-		})
-
-	}
-
-	if (thirdGenre === 28) {
-
-		console.log('inside second')
-
-		fetch("https://api.themoviedb.org/4/list/7434358?api_key=b0b77ea6cc2033f31116d4ef4f5925a6")
-		.then(resp => resp.json())
-		.then(data => {
-
-			const filmArray = data["results"].slice(0, 4);
-
-			for (const film of filmArray) {
-				renderPosters(film, recContainer)
-			}
-		})
-
-	}
-
 }
+
+*/
 
 // as for checking new films against input 
 // we already have a watchedArray, though of course it's nested... 
